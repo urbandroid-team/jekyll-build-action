7a9fcb3e850525d6c1ccdbd0e4f1fbbe894587d2
artaud <richtej2@gmail.com>
2020-07-02 17:12:45 +0200

    remove sudo

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ chmod -R a+w /github/workspace
 # Build the Jekyll site
 ####################################################
 
-sudo jekyll build --trace --config _config.yml,deploy.yml
+jekyll build --trace --config _config.yml,deploy.yml
 #sudo chown -R $USER:$USER /home/runner/work/sleep-docs
 lftp -e 'mirror -R --parallel=20  _site/. /; bye' -u $FTP_USER,$FTP_PASS $FTP_SERVER
 
