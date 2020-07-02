6755123d66797f0dfe4c08e74c0eda453cd5e8df
artaud <richtej2@gmail.com>
2020-07-02 17:16:53 +0200

    add debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,8 @@ chmod -R a+w /github/workspace
 # Build the Jekyll site
 ####################################################
 
+ll
+which lftp
 jekyll build --trace --config _config.yml,deploy.yml
 #sudo chown -R $USER:$USER /home/runner/work/sleep-docs
 lftp -e 'mirror -R --parallel=20  _site/. /; bye' -u $FTP_USER,$FTP_PASS $FTP_SERVER
