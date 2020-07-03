133df888b10e7ff5af4b00c2d2ec74be462ae57e
artaud <richtej2@gmail.com>
2020-07-03 08:07:27 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,10 @@ ls -l
 which lftp
 jekyll build --trace --config /github/workspace/_config.yml,/github/workspace/deploy.yml
 #sudo chown -R $USER:$USER /home/runner/work/sleep-docs
-lftp -e 'mirror -R --parallel=20  _site/. /; bye' -u $FTP_USER,$FTP_PASS $FTP_SERVER
+
+echo "$FTP_USER"
+
+lftp -e 'mirror -R --parallel=20  _site/. /; bye' -u "$FTP_USER","$FTP_PASS" "$FTP_SERVER"
 
 
 ####################################################
